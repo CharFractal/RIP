b2ae6763a09c5d97c399643fd8d78f0769e7d308
Kill Engine <shatterstar999@gmail.com>
2024-07-20 21:11:38 +0530

    Tab Sekection Working
    Tab Selection Wokring

---
+++ Assets/Scripts/UI/TabGroup.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TabGroup : MonoBehaviour
+{
+    public List<CustomTabButton> CustomTabButtons;
+    public Sprite tabIdle;
+    public Sprite tabHover;
+    public Sprite tabActive;
+    public CustomTabButton selectedTab;
+    public List<GameObject> ObjectsToSwap;
+    public void Subscribe(CustomTabButton button)
+    {
+        if (CustomTabButtons == null)
+        {
+            CustomTabButtons = new List<CustomTabButton>();
+        }
+        CustomTabButtons.Add(button);
+    }
+
+
+    public void OnTabEnter(CustomTabButton button)
+    {
+        if (selectedTab == null || button != selectedTab)
+        {
+            ResetTabs();
+            button.background.sprite = tabHover;
+        }
+    }
+
+
+    public void OnTabExit(CustomTabButton button)
+    {
+        ResetTabs();
+    }
+
+
+    public void OnTabSelected(CustomTabButton button)
+    {
+        selectedTab = button;
+        ResetTabs();
+        button.background.sprite = tabActive;
+        int index = button.transform.GetSiblingIndex();
+        for(int i = 0; i < ObjectsToSwap.Count; ++i){
+            if(i == index){
+                ObjectsToSwap[i].SetActive(true);
+            }
+            else{
+                ObjectsToSwap[i].SetActive(false);
+            }
+        }
+    }
+
+    public void ResetTabs()
+    {
+        foreach (CustomTabButton button in CustomTabButtons)
+        {
+            if (selectedTab != null && button == selectedTab) { continue; }
+            button.background.sprite = tabIdle;
+        }
+    }
+}
